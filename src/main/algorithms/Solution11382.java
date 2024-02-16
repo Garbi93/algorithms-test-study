c19682512d74c2dcbd511f6800db7223cb8a7834
Garbi93 <iseung2093@gmail.com>
2024-02-16 14:18:00 +0900

    2/16 정수값 더하기 (Long타입 데이터 주의)

---
+++ src/main/algorithms/Solution11382.java
@@ -0,0 +1,15 @@
+package main.algorithms;
+
+import java.util.Scanner;
+
+// 정수값 더하기 (Long타입 데이터 주의)
+
+public class Solution11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long a = sc.nextLong();
+        Long b = sc.nextLong();
+        Long c = sc.nextLong();
+        System.out.println(a + b + c);
+    }
+}
