d541d8f9ff56ec6591458e862e9ddc64720a5923
Garbi93 <iseung2093@gmail.com>
2024-02-16 12:32:00 +0900

    2/16 사칙연산 및 연산자 우선순위

---
+++ src/main/algorithms/Solution10430.java
@@ -0,0 +1,18 @@
+package main.algorithms;
+
+// 사칙연산 및 연산자 우선순위
+
+import java.util.Scanner;
+
+public class Solution10430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+    }
+}
