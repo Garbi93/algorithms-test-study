f86ee759131390c5f8fdc8129cb04de9add1eefb
Garbi93 <iseung2093@gmail.com>
2024-02-16 12:09:29 +0900

    2/16 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램

---
+++ src/main/algorithms/Solution1008.java
@@ -0,0 +1,14 @@
+package main.algorithms;
+
+import java.util.Scanner;
+
+// 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램
+
+public class Solution1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        System.out.println(a / b);
+    }
+}
