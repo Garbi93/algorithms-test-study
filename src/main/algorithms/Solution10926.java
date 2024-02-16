9ab14555a17da95f351d0e347fa34a9279e60c99
Garbi93 <iseung2093@gmail.com>
2024-02-16 12:18:58 +0900

    2/16 문자열 에 문자열 더하기

---
+++ src/main/algorithms/Solution10926.java
@@ -0,0 +1,13 @@
+package main.algorithms;
+
+import java.util.Scanner;
+
+// 문자열 에 문자열 더하기
+
+public class Solution10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a + "??!");
+    }
+}
