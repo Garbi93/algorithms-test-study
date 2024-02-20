2e3e371c8c5f5cf4e02e4271e0700741b1200b6b
Garbi93 <iseung2093@gmail.com>
2024-02-20 15:54:24 +0900

    2/20 빠르게 입력받고 출력하는 문제

---
+++ src/main/loop/Solution15552.java
@@ -0,0 +1,23 @@
+package main.loop;
+
+// 빠르게 입력받고 출력하는 문제
+
+import java.io.*;
+
+public class Solution15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String[] num = br.readLine().split(" ");
+            int a = Integer.parseInt(num[0]);
+            int b = Integer.parseInt(num[1]);
+            bw.write(String.valueOf(a + b));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
