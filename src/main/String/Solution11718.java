76256ba7890154175640e37fc5b8041e88282c2f
Garbi93 <iseung2093@gmail.com>
2024-06-26 23:01:30 +0900

    Solution11718 그대로 출력하기

--- src/main/String/Solution11718.java
+++ src/main/String/Solution11718.java
@@ -1,27 +1,17 @@
 package main.String;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-// TODO : 이것도 해결 해야함
 public class Solution11718 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<String> listStr = new ArrayList<>();
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
 
-        if (!sc.next().equals("")) {
-            for (int i = 0; i < listStr.size(); i++) {
-                listStr.add(sc.next());
-            }
+            System.out.println(str);
         }
-        for (int i = 0; i < listStr.size(); i++) {
-            System.out.println(listStr.get(i));
-        }
+        sc.close();
 
     }
 }
